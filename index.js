ddf2620339be2cfb75b49bbb5d7c48181f8a10cf
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-09-28 14:43:52 -0700

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record){
+    const result = record.find(obj => obj.result === 'W');
+    if (result === undefined )  {
+    return  undefined
+    }
+    else  { 
+        return result.year
+     }
+ }
